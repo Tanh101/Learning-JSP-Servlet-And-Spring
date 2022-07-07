35ce382c01ca9d8caed0b280a2d79cc1214b9299
Van Tanh Ly <lyvantanh101@gmail.com>
2022-07-07 09:06:01 +0700

    add library decorators

---
+++ src/main/java/com/laptrinhjavaweb/controller/admin/HomeController.java
@@ -0,0 +1,16 @@
+package com.laptrinhjavaweb.controller.admin;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/admin-home"})
+public class HomeController extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+		
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse  response) {
+		
+	}
+}
