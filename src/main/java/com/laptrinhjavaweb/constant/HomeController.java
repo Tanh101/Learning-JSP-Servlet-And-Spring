8e613354df8fd1b2daea7ece41db476c7cfbcd9e
Van Tanh Ly <lyvantanh101@gmail.com>
2022-08-18 16:05:07 +0700

    fix server exceoption

--- src/main/java/com/laptrinhjavaweb/constant/HomeController.java
+++ src/main/java/com/laptrinhjavaweb/constant/HomeController.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = {"/admin-home"})
+@WebServlet(urlPatterns = {"/HomeController"})
 public class HomeController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		RequestDispatcher rd = request.getRequestDispatcher("/views/admin/home.jsp");
